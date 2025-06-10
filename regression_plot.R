4e7da66643770ba01c104b4497df34da24876b35
sonia-ahmaripour <sonia.ahmaripour.da@gmail.com>
2025-06-10 01:09:18 -0400

    Delete regression_plot.R

--- regression_plot.R
+++
@@ -1,7 +0,0 @@
-reg_plot <- ggplot(SmashData, aes(x = GAttack, y = TWins)) +
-  geom_point(alpha = 0.7) +
-  geom_smooth(method = "glm", method.args = list(family = "binomial")) +
-  theme_minimal() +
-  labs(title = "Logistic Regression: GAttack vs TWins")
-
-ggsave("visuals/regression_gattack_twins.png", plot = reg_plot, width = 6, height = 4)
